4694d5397a23ccf6912896444d67592c4bf9574b
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-02-05 23:16:07 +0800

    version update

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: undefined-field
 --[[______  __
   / ____/ | / / By: GNamimates
- / / __/  |/ / GNUI vF3.4.2
+ / / __/  |/ / GNUI vF3.4.3
 / /_/ / /|  / A high level UI library for figura.
 \____/_/ |_/ Stable Release: https://github.com/lua-gods/GNUI, Unstable Pre-release: https://github.com/lua-gods/GNs-Avatar-3/blob/main/libraries/gnui.lua]]
 
