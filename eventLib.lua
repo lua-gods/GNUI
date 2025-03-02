53647240dcdfe14ee157e688ce06120ad0fa8441
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-03-02 20:51:51 +0800

    compacted eventLib

--- eventLib.lua
+++ eventLib.lua
@@ -1,63 +1,47 @@
--- FROM: auriafoxgirl (note: old library)
----@class EventLibAPI
-local lib = {}
 
 ---@class EventLib
-local eventMetatable = {__type = "Event", __index = {}}
-local eventsMetatable = {__index = {}}
-eventMetatable.__index = eventMetatable
+local EventLib = {}
+EventLib.__index = EventLib
 
----@return EventLib
-function lib.new()
-  return setmetatable({subscribers = {}}, eventMetatable)
-end
----@return EventLib
-function lib.newEvent()
-  return setmetatable({subscribers = {}}, eventMetatable)
+function EventLib.new()
+	return setmetatable({}, EventLib)
 end
 
-function lib.table(tbl)
-  return setmetatable({_table = tbl or {}}, eventsMetatable)
+EventLib.newEvent = EventLib.new
+
+function EventLib:register(func, name)
+	self[name or func] = func
 end
 
----Registers an event
----@param func function
----@param name string?
-function eventMetatable:register(func, name)
-  if name then
-    self.subscribers[name] = {func = func}
-  else
-    table.insert(self.subscribers, {func = func})
-  end
+function EventLib:clear()
+	for key in pairs(self) do self[key] = nil end
 end
 
----Clears all event
-function eventMetatable:clear() self.subscribers = {} end
-
----Removes an event with the given name.
----@param match string
-function eventMetatable:remove(match) self.subscribers[match] = nil end
-
----Returns how much listerners there are.
----@return integer
-function eventMetatable:getRegisteredCount() return #self.subscribers end
-
-function eventMetatable:__call(...) local returnValue = {} for _, data in pairs(self.subscribers) do table.insert(returnValue, {data.func(...)}) end return returnValue end
-
-function eventMetatable:invoke(...) local returnValue = {} for _, data in pairs(self.subscribers) do table.insert(returnValue, {data.func(...)}) end return returnValue end
-
-function eventMetatable:__len() return #self.subscribers end
-
--- events table
-function eventsMetatable.__index(t, i) return t._table[i] or (type(i) == "string" and getmetatable(t._table[i:upper()]) == eventMetatable) and t._table[i:upper()] or nil end
-
-function eventsMetatable.__newindex(t, i, v)
-  if type(i) == "string" and type(v) == "function" and t._table[i:upper()] and getmetatable(t._table[i:upper()]) == eventMetatable then t._table[i:upper()]:register(v)
-  else t._table[i] = v end
+function EventLib:remove(name)
+	self[name] = nil
 end
 
-function eventsMetatable.__ipairs(t) return ipairs(t._table) end
-function eventsMetatable.__pairs(t) return pairs(t._table) end
+function EventLib:getRegisteredCount() return #self end
 
--- return library
-return lib
+function EventLib:__len() return #self end
+
+function EventLib:__call(...)
+	local flush = {}
+	for _, func in pairs(self) do
+		flush[#flush+1] = {func(...)}
+	end
+	return flush
+end
+
+---@type fun(self: EventLib, ...: any): any[]
+EventLib.invoke = EventLib.__call
+
+function EventLib.__index(t, i)
+	return rawget(t,i)or rawget(t,i:upper()) or EventLib[i]
+end
+
+function EventLib.__newindex(t, i, v)
+	rawset(t,type(i) == "string" and t[i:upper()] or i,v)
+end
+
+return EventLib
