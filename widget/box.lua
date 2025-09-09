d088c39c4740a176102e58909f85e2f5f41e88c2
GN68 <81744903+GN68@users.noreply.github.com>
2025-09-10 00:57:26 +0800

    added BoxAPI to keep consistency and avoid circular dependencies

---
+++ widget/box.lua
@@ -0,0 +1,24 @@
+local Box = require("./../prims/box") ---@type GNUI.Box
+local cfg = require("./../config") ---@type GNUI.Config
+local Event = cfg.event ---@type Event
+local utils = cfg.utils ---@type GNUI.UtilsAPI
+local Theme = require("./../theme") ---@type GNUI.ThemeAPI
+
+
+---@class GNUI.BoxAPI : GNUI.Box
+local BoxAPI = {}
+
+function BoxAPI.new(parent, variant)
+	local box = Box.new(parent)
+	if variant then
+		local sprite = Theme.getStyle(box, "backdrop", variant)
+		if sprite then
+			box:setSprite(sprite)
+		end
+	end
+	return box
+end
+
+BoxAPI.__index = Box.__index
+
+return BoxAPI
