9e2fc4e2c74d2c26b13d12cc302e0e0ea706f6c7
GN68 <81744903+GN68@users.noreply.github.com>
2025-09-10 19:14:11 +0800

    fixed slider not accounting for anchor when dragging the knob

--- widget/slider.lua
+++ widget/slider.lua
@@ -122,7 +122,7 @@ function SliderAPI.new(parent,config,variant)
 	---@param event GNUI.InputEventMouseMotion
 	self.MOUSE_MOVED:register(function (event)
 		if self.isPressed then
-				local pos = self:toLocal(event.pos)/self:getDimensionSize()
+				local pos = self:toLocal(event.pos)/self:getFinalSize()
 				local gsize = self.cache.grabber_size or 0
 				if self.isVertical then
 					self:setValue(math.map(pos.y,gsize,1-gsize,self.min,self.max))
