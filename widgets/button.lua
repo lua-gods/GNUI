c3d4e280c029ef622e89867945f2bb59c4aa0070
GN68 <81744903+GN68@users.noreply.github.com>
2026-01-10 02:05:47 +0800

    added button events and styling

--- widgets/button.lua
+++ widgets/button.lua
@@ -3,14 +3,22 @@ local Box = require("../core/prims/box") ---@type GNUI.BoxAPI
 local Event = require("../" .. config.EVENT)
 
 
+local Style = require("../" .. config.STYLE) ---@type GNUI.StyleAPI
+local Layout = require("../" .. config.LAYOUT) ---@class GNUI.LayoutAPI
+
+
 ---@class GNUI.ButtonAPI
 local ButtonAPI = {}
 
 
 ---@class GNUI.Button : GNUI.Box
----@field pressed boolean
+---@field down boolean
+---
 ---@field PRESSED Event
 local Button = {}
+Button.__index = function (t,i)
+	return rawget(t,i) or Button[i] or Box.index(i)
+end
 Button.__style = "button"
 Button.__type = "Button"
 
@@ -20,21 +28,39 @@ function ButtonAPI.new(canvas)
 	local self = Box.new(canvas)
 	---@cast self GNUI.Button
 	
-	self.pressed = false
+	self.down = false
 	self.PRESSED = Event.new()
 	self.MOUSE_INPUT:register(function (button, state)
-		if button == 1 then
+		if button == 0 then
 			if state == 1 then
-				self.pressed = true
-			elseif state == 0 and self.pressed then
+				self.down = true
+				self.sprite:setStyle(Style.getStyle(self,self.variant,"pressed"))
+			elseif state == 0 and self.down then
 				self.PRESSED:invoke()
-				self.pressed = false
+				self.down = false
+				self.sprite:setStyle(Style.getStyle(self,self.variant,"normal"))
 			end
 		end
 	end)
-	
+	setmetatable(self, Button)
 	return self
 end
+
+
+--────────────────────────-< Layout Parser >-────────────────────────--
+
+
+---@param layout any
+---@param canvas GNUI.Canvas
+---@param button GNUI.Button?
+---@return GNUI.Button
+function ButtonAPI.parse(layout,canvas,button)
+	local box = button or Box.parse(layout,canvas,ButtonAPI.new(canvas))
+
+	return box
+end
+
+Layout.registerType("button", ButtonAPI.parse)
 
 
 return ButtonAPI
