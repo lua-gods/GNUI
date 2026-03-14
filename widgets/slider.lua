329d8a994248f696ff90fd8f0cdba274735edae7
GN68 <81744903+GN68@users.noreply.github.com>
2026-03-14 15:51:43 +0800

    added comments to slider (still wip)

--- widgets/slider.lua
+++ widgets/slider.lua
@@ -1,14 +1,14 @@
 local BASE = ((...):gsub("/",".")):match(".+%.GNUI")
-local paths = require(BASE..".paths") ---@type GNUI.config
+local cfg = require(BASE..".config") ---@type GNUI.config
 
-local Box = require(paths.WIDGETS..".box") ---@type GNUI.BoxAPI
-local Event = require(paths.EVENT)
-local Button = require(paths.WIDGETS..".button") ---@type GNUI.Widget.ButtonAPI
+local Box = require(cfg.WIDGETS..".box") ---@type GNUI.BoxAPI
+local Event = require(cfg.EVENT)
+local Button = require(cfg.WIDGETS..".button") ---@type GNUI.Widget.ButtonAPI
 
-local Style = require(paths.THEME..".init") ---@type GNUI.ThemeAPI
-local Layout = require(paths.LAYOUT..".init") ---@type GNUI.LayoutAPI
+local Style = require(cfg.THEME..".init") ---@type GNUI.ThemeAPI
+local Layout = require(cfg.LAYOUT..".init") ---@type GNUI.LayoutAPI
 
-local utils = require(paths.UTILS) ---@type GNUI.utils
+local utils = require(cfg.UTILS) ---@type GNUI.utils
 
 ---@class Event.GNUI.Button.ValueChanged : Event
 ---@field register fun(self,func:fun(value: number))
@@ -18,16 +18,17 @@ local utils = require(paths.UTILS) ---@type GNUI.utils
 local SliderAPI = {}
 
 
+---A widget that allows users to select a single value or a range of predefined spectrum  by dragging a handle (thumb) along a bar.
 ---@class GNUI.Widget.Slider
----@field value number
----@field min number
----@field max number
----@field step number
----@field softBoundary boolean Tells if the slider is allowed to go out of bounds
----@field suffix string
----@field prefix string
----@field VALUE_CHANGED Event.GNUI.Button.ValueChanged
----@field PRESSED Event.GNUI.Button.PRESSED
+---@field value number # The default value
+---@field min number # the minimum allowed value
+---@field max number # the maximum allowed value
+---@field step number # the step size of the slider, 0 for none
+---@field softBoundary boolean #Tells if the slider is allowed to go out of bounds
+---@field prefix string # the prefix when the value is displayed
+---@field suffix string # the suffix when the value is displayed
+---@field VALUE_CHANGED Event.GNUI.Button.ValueChanged # triggered when the value is changed
+---@field PRESSED Event.GNUI.Button.PRESSED # triggered when the slider is pressed
 local Slider = {}
 Slider.__index = function (t,i)
 	return rawget(t,i)
